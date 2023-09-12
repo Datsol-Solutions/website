36e9f61d64fc350b20962ffb4a507becdc287207
Archisha394 <guptaarchisha394@kgpian.iitkgp.ac.in>
2023-09-13 00:33:16 +0530

    bgvideo

---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import videoBg from './bg1.mp4';
+ const Main = () => {
+    return (
+        <div className='main'>
+            <video src={videoBg}/>
+
+        </div>
+    )
+ }
--- src/VideoBackground.js
+++ src/VideoBackground.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import { VideoCover } from 'react-video-cover'; // Corrected import
+//import { VideoCover } from 'react-video-cover'; // Corrected import
 import './Video.css';
+import video from './bg1.mp4'
 
 function BackgroundVideo() {
-  const videoOptions = {
-    src: 'bg1.mp4', // Replace with the path to your video file
-    autoPlay: true,
-    loop: true,
-    muted: true,
-  };
-
+  // const videoOptions = {
+  //   src: {video}, // Replace with the path to your video file
+  //   autoPlay: true,
+  //   loop: true,
+  //   muted: true,
+  // };
+  
   return (
-    <section className="background-video-section">
-      <VideoCover videoOptions={videoOptions} />
-      {/* Add other content or components you want to display on top of the video */}
-      <div className="content">
+    <section>
+       <div className='main'>
+
+        <video src={video} autoPlay loop muted/>
+
+    </div>
+    <div className="content">
         <h1>Welcome to Our Website</h1>
-        <p>Discover amazing content here!</p>
+         <p>Discover amazing content here!</p>
       </div>
     </section>
-  );
+    
+  )
+
+  // return (
+  //   <section className="background-video-section">
+  //     <VideoCover videoOptions={videoOptions} />
+  //     {/* Add other content or components you want to display on top of the video */}
+  //     <div className="content">
+  //       <h1>Welcome to Our Website</h1>
+  //       <p>Discover amazing content here!</p>
+  //     </div>
+  //   </section>
+  // );
 }
 
 export default BackgroundVideo;
