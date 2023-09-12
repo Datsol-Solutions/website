a3f2cf8a88f547a0ed63d05d6c4617d60806b5bb
Archisha394 <guptaarchisha394@kgpian.iitkgp.ac.in>
2023-09-13 00:33:16 +0530

    bgvideo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BackgroundVideo from './VideoBackground';
 import Navibar from './Navibar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Main from './Main';
 function App() {
   return (
     <Router>
       <Navibar />
-      <Switch>
-        <Route path="/" exact>
-          <React.Fragment>
-            <BackgroundVideo />
-            
-          </React.Fragment>
-        </Route>
-        {/* Define other routes here */}
-      </Switch>
+      <div>
+        <BackgroundVideo/>
+      </div>
     </Router>
   );
 }
