60cb200109ef66ec83491b54be21511052808ef0
Archisha394 <guptaarchisha394@kgpian.iitkgp.ac.in>
2023-09-13 16:01:50 +0530

    video dimensions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BackgroundVideo from './VideoBackground';
 import Navibar from './Navibar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Main from './Main';
 function App() {
   return (
     <Router>
--- src/VideoBackground.js
+++ src/VideoBackground.js
@@ -4,12 +4,12 @@ import './Video.css';
 import video from './bg1.mp4'
 
 function BackgroundVideo() {
-  // const videoOptions = {
-  //   src: {video}, // Replace with the path to your video file
-  //   autoPlay: true,
-  //   loop: true,
-  //   muted: true,
-  // };
+   /* const videoOptions = {
+     src: {video}, // Replace with the path to your video file
+     autoPlay: true,
+     loop: true,
+     muted: true,
+   }; */
   
   return (
     <section>
@@ -20,22 +20,23 @@ function BackgroundVideo() {
     </div>
     <div className="content">
         <h1>Welcome to Our Website</h1>
-         <p>Discover amazing content here!</p>
+         <h5>Discover amazing content here!</h5>
       </div>
     </section>
     
   )
 
-  // return (
-  //   <section className="background-video-section">
-  //     <VideoCover videoOptions={videoOptions} />
-  //     {/* Add other content or components you want to display on top of the video */}
-  //     <div className="content">
-  //       <h1>Welcome to Our Website</h1>
-  //       <p>Discover amazing content here!</p>
-  //     </div>
-  //   </section>
-  // );
+   /* return (
+     <section className="background-video-section">
+       <VideoCover videoOptions={videoOptions} />
+       // Add other content or components you want to display on top of the video 
+       <div className="content">
+         <h1>Welcome to Our Website</h1>
+         <p>Discover amazing content here!</p>
+       </div>
+     </section>
+   ); 
+   */
 }
 
 export default BackgroundVideo;
