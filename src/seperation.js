46ec1d2f6be65a30135c609b278297758d9fe1cd
Harshitha <harshithanakda2002@gmail.com>
2023-10-02 19:03:16 +0530

    separate

--- src/seperation.js
+++ src/seperation.js
@@ -28,26 +28,15 @@ function ParallaxEffect() {
     backgroundSize: 'cover',
     backgroundPosition: `center ${backgroundPositionY}`,
     backgroundAttachment: 'fixed',
-    minHeight: '100vh',
+    minHeight: '70vh',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   };
 
-  const contentStyle = {
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    padding: '20px',
-    borderRadius: '8px',
-    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)',
-  };
-
   return (
     <div style={containerStyle}>
-      <div style={contentStyle}>
-        <h1>Your Content Goes Here</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        {/* Add your content here */}
-      </div>
+
     </div>
   );
 }
