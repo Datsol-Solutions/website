35c07173cad4e73bcb032670be3bb33bba56e65d
Harshitha <harshithanakda2002@gmail.com>
2023-10-03 23:39:04 +0530

    con

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,6 +1,7 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './ContactUs.css'; // Import your CSS file with styles
 import Logo from './logo.png'; // Import your logo image
+import { GoogleSpreadsheet } from 'google-spreadsheet'; // Import the Google Spreadsheet library
 
 const style1 = {
   fontFamily: 'Rokkitt, sans-serif',
@@ -11,29 +12,65 @@ const style2 = {
 };
 
 function ContactUs() {
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const phoneRef = useRef(); // Add a reference for the phone number input
-  const queryRef = useRef(); // Add a reference for the query input
-  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
 
+  const {
+    REACT_APP_PRIVATE_KEY,
+    REACT_APP_CLIENT_EMAIL,
+    REACT_APP_SPREADSHEET_ID,
+    REACT_APP_SHEET_ID
+  } = process.env;
+
+  const doc = new GoogleSpreadsheet(REACT_APP_SPREADSHEET_ID);
+
+  const appendSpreadsheet = async (row) => {
+    try {
+      await doc.useServiceAccountAuth({
+        client_email: REACT_APP_CLIENT_EMAIL,
+        private_key: REACT_APP_PRIVATE_KEY,
+      });
+
+      await doc.loadInfo();
+
+      const sheet = doc.sheetsById[REACT_APP_SHEET_ID];
+
+      const result = await sheet.addRow(row);
+
+      return result;
+    } catch (e) {
+      console.error('Error: ', e);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    appendSpreadsheet(formData);
+  };
+
+  // Keep the commented code for reference
+  /*
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     // Create an object to hold the form data
     const formData = {
       name: nameRef.current.value,
-      email: emailRef.current.value,
-      phone: phoneRef.current.value, // Get the phone number value
-      query: queryRef.current.value, // Get the query value
+      businessEmail: businessEmailRef.current.value,
+      businessName: businessNameRef.current.value,
+      phone: phoneRef.current.value,
+      message: messageRef.current.value,
     };
 
-    // Send the formData object to your Google Sheets using a fetch request or any preferred method
+    // Send the formData object to your Google Sheets using a fetch request
     try {
       setLoading(true);
 
-      // Use the fetch API to send the form data to your server or Google Sheets
-      const response = await fetch('/api/submit-contact-form', {
+      // Replace 'YOUR_GOOGLE_APPS_SCRIPT_URL' with your Google Apps Script endpoint URL
+      const response = await fetch('https://script.google.com/macros/s/AKfycbxvNidV0cwn_Ovnl9PRuEYyc1eGvpxNbyd9tNBBTKSuPSzGuvWZlYrA0rn3NArAwYkN/exec', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -52,6 +89,7 @@ function ContactUs() {
       setLoading(false);
     }
   };
+  */
 
   return (
     <div className="contact-us-container">
@@ -60,25 +98,79 @@ function ContactUs() {
       </div>
       <div className="form-container">
         <h2 style={style1}>Contact Us</h2>
-        <form className="for" onSubmit={handleSubmit}>
+        <form method="post" className="for" onSubmit={handleSubmit}>
           <div className="form-group">
-            <label htmlFor="name" style={style1}>Name:</label>
-            <input ref={nameRef} style={style2} type="text" id="name" placeholder="Enter your name" required />
+            <label htmlFor="name" style={style1}>
+              Name:
+            </label>
+            <input
+              name="name"
+              style={style2}
+              type="text"
+              id="name"
+              placeholder="Enter your name"
+              required
+              onChange={handleInputChange}
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="email" style={style1}>Email:</label>
-            <input ref={emailRef} style={style2} type="email" id="email" placeholder="Enter your email" required />
+            <label htmlFor="businessEmail" style={style1}>
+              Business Email:
+            </label>
+            <input
+              name="businessEmail"
+              style={style2}
+              type="email"
+              id="businessEmail"
+              placeholder="Enter your business email"
+              required
+              onChange={handleInputChange}
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="phone" style={style1}>Phone Number:</label>
-            <input ref={phoneRef} style={style2} type="tel" id="phone" placeholder="Enter your phone number" required />
+            <label htmlFor="businessName" style={style1}>
+              Business Name:
+            </label>
+            <input
+              name="businessName"
+              style={style2}
+              type="text"
+              id="businessName"
+              placeholder="Enter your business name"
+              required
+              onChange={handleInputChange}
+            />
           </div>
           <div className="form-group">
-            <label htmlFor="query" style={style1}>Query:</label>
-            <textarea ref={queryRef} style={style2} id="query" placeholder="Enter your query" rows="4" required />
+            <label htmlFor="phone" style={style1}>
+              Phone Number:
+            </label>
+            <input
+              name="phone"
+              style={style2}
+              type="tel"
+              id="phone"
+              placeholder="Enter your phone number"
+              required
+              onChange={handleInputChange}
+            />
           </div>
           <div className="form-group">
-            <button className="btn" style={style1} disabled={loading} type="submit">
+            <label htmlFor="message" style={style1}>
+              Message:
+            </label>
+            <textarea
+              name="message"
+              style={style2}
+              id="message"
+              placeholder="Enter your message"
+              rows="4"
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn" style={style1} type="submit">
               Submit
             </button>
           </div>
--- src/server.js
+++ src/server.js
@@ -1,61 +1,49 @@
-// server.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const { google } = require('googleapis');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-// Set up Google Sheets API
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
-const spreadsheetId = '1XPE7D7GDNYRijkm_ZrrNQvVlRAxV4EViFkNXeIXeB34'; // Replace with your Google Sheets document ID
+// Load the credentials from your JSON key file
+const keyFile = require('./datsol-h-35038aa76b14.json'); // Replace with the correct path to your JSON key file
 
+// Initialize the Google Sheets API client
 const sheets = google.sheets('v4');
-const sheetsAuth = new google.auth.GoogleAuth({
-  keyFile: 'datsol-38eb4e32a2c7.json', // Replace with the path to your JSON key file
-  scopes: SCOPES,
+const auth = new google.auth.GoogleAuth({
+  credentials: keyFile,
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 
 // Define a route to handle form submissions
 app.post('/api/submit-contact-form', async (req, res) => {
   try {
-    // Extract form data from the request body
-    const { name, email, phone, query } = req.body;
-
-    // Authenticate with Google Sheets API
-    const auth = await sheetsAuth.getClient();
-
-    // Append the form data to the Google Sheets
-    const values = [[name, email, phone, query]];
+    const { name, businessEmail, businessName, phoneNumber, message } = req.body;
     const sheetsResponse = await sheets.spreadsheets.values.append({
-      auth,
-      spreadsheetId,
-      range: 'DATSOL contact us', // Replace with your sheet name or range
-      valueInputOption: 'USER_ENTERED',
+      auth: await auth.getClient(),
+      spreadsheetId: '1Q6zVgheVzM4VqJclIo9SxI5Nj1A6vBN4Gapwt37Psew',
+      range: 'Form Responses 1', // Replace with your sheet name or range
+      valueInputOption: 'RAW',
       resource: {
-        values,
+        values: [[name, businessEmail, businessName, phoneNumber, message]],
       },
     });
 
-    // Check if the data was successfully appended
     if (sheetsResponse.status === 200) {
       console.log('Data appended to Google Sheets.');
-      // Send a success response to the client
       res.status(200).send('Form submitted successfully.');
     } else {
       console.error('Error appending data to Google Sheets.');
-      // Send an error response to the client
       res.status(500).send('Error submitting the form.');
     }
   } catch (error) {
-    console.error(error);
-    res.status(500).send('Error submitting the form.');
+    console.error('Error submitting form:', error);
+    res.status(500).send('Error submitting the form. Please check the server logs for more information.');
   }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
