4d815f9672ae98df0a1be25392fc76052e993edb
imkhushi <99580281+imkhushi@users.noreply.github.com>
2023-10-09 20:25:43 +0530

    about-us

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -19,21 +19,21 @@ const boxesData = [
   {
     image: image1,
     heading: 'WHO WE ARE?' ,
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    text: '"Lorem ipsum dollabore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
   {
     image: image2,
     heading: 'WHAT WE DO?',
-    text: 'Nulla quis lorem ut libero malesuada feugiat.',
+    text: '"Loreabore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
   {
     image: image3,
     heading: 'HOW CAN WE HELP YOU?',
-    text: 'Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.',
+    text: '"Lorem ipsum dolor sit amet, conselaboregna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
   },
 ];
 
-function Box({ image, heading, text, delay, animation }) {
+function Box({ image, heading, text, delay, animation, layout }) {
   useEffect(() => {
     AOS.init({
       duration: 1000,
@@ -43,14 +43,15 @@ function Box({ image, heading, text, delay, animation }) {
 
   return (
     <div
-      className={`box ${animation}`}
+      className={`box ${animation} ${layout === 'right' ? 'box-right' : ''}`}
       style={{ animationDelay: `${delay}ms` }}
       data-aos={animation}
     >
+      <div className={`info ${layout === 'right' ? 'info-right' : ''}`}>
+        <h2 className='textual' data-aos="fade-up">{heading}</h2>
+        <p className='textual' data-aos="fade-up">{text}</p>
+      </div>
       <img className='images' src={image} alt={heading} data-aos="fade-up" />
-      <div className='info'><h2 data-aos="fade-up">{heading}</h2>
-      <p data-aos="fade-up">{text}</p></div>
-      
     </div>
   );
 }
@@ -67,6 +68,7 @@ function AboutUs() {
           text={box.text}
           delay={index * 1000}
           animation={index === 1 ? 'fade-right' : 'fade-left'}
+          layout={index === 1 ? 'right' : 'left'}
         />
       ))}
     </div>
@@ -78,8 +80,8 @@ Box.propTypes = {
   heading: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
   delay: PropTypes.number.isRequired,
-  animationDirection: PropTypes.string.isRequired,
   animation: PropTypes.string.isRequired,
+  layout: PropTypes.string.isRequired,
 };
 
 export default AboutUs;
