f937c31e438c3c59a076b82c12186dd292e6cea2
imkhushi <99580281+imkhushi@users.noreply.github.com>
2023-10-13 23:42:23 +0530

    ourteam

--- src/Team_card.js
+++ src/Team_card.js
@@ -11,6 +11,14 @@ const style2 = {
 };
 
 function Teamcard(props) {
+  const handleGmailClick = (event) => {
+    // Prevent the default behavior (page refresh) of the anchor element
+    event.preventDefault();
+
+    // Display an alert message with the email ID link
+    window.alert(`Email ID: ${props.gmaillink}`);
+  };
+
   return (
     <div className="card">
       <div className="card-info">
@@ -20,10 +28,10 @@ function Teamcard(props) {
       </div>
       <ul className="card-social">
         <li className="card-social__item">
-        <a href={props.gmaillink}>
-          <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"></path>
-          </svg>
+          <a href={props.gmaillink} onClick={handleGmailClick}>
+            <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L5.455 4.64 12 9.548l6.545-4.91 1.528-1.145C21.69 2.28 24 3.434 24 5.457z"></path>
+            </svg>
           </a>
         </li>
         <li className="card-social__item">
@@ -42,8 +50,8 @@ Teamcard.propTypes = {
   name: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
-  inlink: PropTypes.string.isRequired, // Add this line for the 'inlink' prop
-  gmaillink: PropTypes.string.isRequired
+  inlink: PropTypes.string.isRequired,
+  gmaillink: PropTypes.string.isRequired,
 };
 
 export default Teamcard;
