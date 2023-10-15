8bd5b05b513a410f90791b0e24af201b44002c04
imkhushi <99580281+imkhushi@users.noreply.github.com>
2023-10-15 19:57:52 +0530

    hello

--- src/Achievement.js
+++ src/Achievement.js
@@ -20,16 +20,12 @@ const AchievementSection = () => {
   const sectionRef = useRef(null);
 
   const data = [
-    { country: "cn", value: 1389618778 }, // china
-    { country: "in", value: 1311559204 }, // india
-    { country: "us", value: 331883986 }, // united states
-    { country: "id", value: 264935824 }, // indonesia
-    { country: "pk", value: 210797836 }, // pakistan
-    { country: "br", value: 210301591 }, // brazil
-    { country: "ng", value: 208679114 }, // nigeria
-    { country: "bd", value: 161062905 }, // bangladesh
-    { country: "ru", value: 141944641 }, // russia
-    { country: "mx", value: 127318112 }, // mexico
+    { country: "gb", value: 1 }, // UK
+    { country: "in", value: 1 }, // india
+    { country: "us", value: 1 }, // united states
+    { country: "th", value: 1 }, // thailand
+    { country: "za", value: 1 }, // south africa
+
   ];
 
   useEffect(() => {
@@ -95,12 +91,11 @@ const AchievementSection = () => {
     <div id="achievement-section" className="achievement-section" ref={sectionRef}>
       <div className="map-gif">
       <WorldMap
-        color="Blue"
-        title="Our Clients"
+        color="#1e6dad"
+        title=""
         value-suffix="people"
         size="lg"
         data={data}
-
       />
       </div>
       <div className="text-content">
