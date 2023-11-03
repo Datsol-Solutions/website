ebcc98566378e086c8bc086247db2cf026580fc0
Archisha394 <guptaarchisha394@kgpian.iitkgp.ac.in>
2023-11-03 19:59:05 +0530

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import './Footer.css';
+import email from './media/email.png';
+import dial from './media/phone.png';
 const style1 = {
   fontFamily: 'Rokkitt, sans-serif',
 };
@@ -14,19 +16,33 @@ const Footer = () => {
   return (
     <footer className="footer" style={style2}>
       <div className="footer-columns">
+      <div className="footer-column">
+        <p>Datsol Solutions <br/>IIT Kharagpur, India - 721302</p>
+          <p className='items'>
+            <a className='maillink' href="mailto:datsolsolutions@gmail.com?subject=">
+              <img className='dial-icon' src={email} alt="Email" /> datsolsolutions@gmail.com
+            </a>
+          </p>
+          <a className='items' href="tel:7821040764">
+            <img className='dial-icon' src={dial} alt="Phone" /> 7821040764
+          </a>
+          <a className='items' href="tel:7060529746">
+            <img className='dial-icon' src={dial} alt="Phone" /> 7060529746
+          </a>
+          
+          
+        </div>
+      
         <div className="footer-column">
           <a className='items' href="#home">Back to top</a>
           <a className='items' href="#about">About Us</a>
+          <a className='items' href="#achievements">Achievements</a>
           <a className='items' href="#services">Services</a>
           <a className='items' href="#ourteam">Our Team</a>
         </div>
-
-        <div className="footer-column">
-          <p className='items'>Mail-<a className='maillink' href="mailto:datsolsolutions@gmail.com?subject="> datsolsolutions@gmail.com</a></p>
-          <p className='items'>Ph No.- 7821040764</p>
-          <p className='items'>Ph No.- 7060529746</p>
         </div>
-      </div>
+
+        
 
       <hr className="footer-divider" />
       <p className="footer-company">Copyright © 2023datsolsolutions.com</p>
